ed20ee88c05898b5aabd253ab08b53f8e08420a5
Vijay Gupta <vijayg963@gmail.com>
2022-05-25 10:55:00 +0530

    Assignment-3

---
+++ block-BNaaej/book-store/models/Book.js
@@ -0,0 +1,18 @@
+let mongoose = require('mongoose');
+let Schema = mongoose.Schema;
+
+let book = new Schema(
+  {
+    title: { type: String },
+    discription: { type: String },
+    author: { type: String },
+    category: [{ type: String }],
+    tags: [{ type: String }],
+    commentId: [{ type: Schema.Types.ObjectId, ref: 'Comment' }],
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model('Book', book);
